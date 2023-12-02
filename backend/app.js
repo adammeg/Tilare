49e7988d6ce3a0833211e6a9ca9e8dde9968aec4
adammeg <adam.bhedj13@gmail.com>
2023-12-02 16:14:18 +0100

    first commit

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var path = require('path');
+const mongoose = require("mongoose");
+var cors = require('cors')
+
+// IMPORT ROUTES
+
+
+var app = express();
+app.use(cors())
+// SERVER PORT
+const port = process.env.PORT || 3000;
+
+// DATA BASE CONNECTION
+mongoose.connect(
+  "mongodb+srv://adambenhadjaissa:tylar123@tylarcluster.rsonrk6.mongodb.net/?retryWrites=true&w=majority")
+.then(() => {
+  console.log("Database connected!");
+  // Starting a server
+  app.listen(port, process.env.ALWAYSDATA_HTTP_ID, () => {
+    console.log(`App is running at ${port}`);
+  });
+})
+.catch((err) => console.log(err));
+
+// for cors origin config
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
